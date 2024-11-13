10a8cc82efaa7070bf72e8c9c5c632aa1fe7438d
NicolasB <nicolas.blin@estaca.eu>
2024-11-13 16:59:01 +0100

    black reformatting

--- src/main.py
+++ src/main.py
@@ -2,6 +2,7 @@ import sys
 import webbrowser
 from PyQt5.QtWidgets import QApplication, QWidget, QPushButton, QVBoxLayout
 
+
 class RickRollApp(QWidget):
     def __init__(self):
         super().__init__()
@@ -13,7 +14,9 @@ class RickRollApp(QWidget):
 
         # Create a button
         self.button = QPushButton("Click me!", self)
-        self.button.clicked.connect(self.rickroll)  # Connect button click to the rickroll function
+        self.button.clicked.connect(
+            self.rickroll
+        )  # Connect button click to the rickroll function
 
         # Add the button to a layout
         layout = QVBoxLayout()
@@ -23,13 +26,15 @@ class RickRollApp(QWidget):
     def rickroll(self):
         # url of never gonna give you up
         url = "https://www.youtube.com/watch?v=dQw4w9WgXcQ"
-        webbrowser.open(url) #open in default browser
+        webbrowser.open(url)  # open in default browser
+
 
 def main():
     app = QApplication(sys.argv)
     window = RickRollApp()
     window.show()
     sys.exit(app.exec_())
+
 
 if __name__ == "__main__":
     main()
